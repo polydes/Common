4abcdb184dfec8135f8197f474126485b9bd1eb2
Justin Espedal <justin.espedal@gmail.com>
2017-09-29 02:29:33 -0700

    Snippets automatically have default icon in Stencyl 3.5

--- src/com/polydes/common/sw/Snippets.java
+++ src/com/polydes/common/sw/Snippets.java
@@ -3,13 +3,10 @@ package com.polydes.common.sw;
 import java.io.File;
 import java.io.IOException;
 
-import javax.swing.ImageIcon;
-
 import stencyl.core.engine.snippet.ISnippet;
 import stencyl.core.engine.snippet.ISnippet.SnippetType;
 import stencyl.core.lib.Game;
 import stencyl.sw.util.FileHelper;
-import stencyl.sw.util.Loader;
 import stencyl.sw.util.Locations;
 
 public class Snippets
@@ -40,12 +37,6 @@ public class Snippets
 			);
 			
 			game.getSnippetList().put(ID, s);
-			
-			String url = Locations.getPath("snippets") + ID + ".png";
-			ImageIcon icon = Loader.loadIcon("res/snippet/icon.png");
-			s.setIcon(icon.getImage());
-			byte[] bytes = FileHelper.readImageToByteArray(icon.getImage(), "png");
-			FileHelper.writeToGameFile(game, url, bytes);
 			
 			new File(Locations.getPath(Locations.getGameLocation(game), "code")).mkdirs();
 			FileHelper.writeToGameFile(game, Locations.getPath("code") + className + ".hx", sourceCode.getBytes());
